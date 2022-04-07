85c71b9c62be27b64ff2be0aa5e9691753b4400f
AndresMarolt <andresmmarolt@gmail.com>
2022-04-07 20:17:13 -0300

    desafio_ItemDetail 7/4

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,17 +3,19 @@ import './Item.css'
 const Item = ({equipo, jugador, tipo, precio, img}) => {
     return(
         <div className="Producto">
-            <div className="Producto__img">
-                <img src={img} alt={jugador} className="Producto__data-img" />
-            </div>
+            <div>
+                <div className="Producto__img">
+                    <img src={img} alt={jugador} />
+                </div>
 
-            <div className="Producto__comprar">
-                <p className="Producto__comprar-titulo">{equipo} - {jugador} [{tipo}]</p>
-                <p className="Producto__comprar-precio">{precio}</p>
+                <div className="Producto__comprar">
+                    <p className="Producto__comprar-precio">{precio}</p>
+                    <p className="Producto__comprar-titulo">{equipo} - {jugador} [{tipo}]</p>
+                </div>
+
             </div>
 
             <button className="Btn-info">Ver Más</button>
-
         </div>
     )
 }
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,16 +1,21 @@
-
+import './ItemDetail.css'
 
 
 const ItemDetail = ({categoria, equipo, tipo, img, jugador, precio, descripcion}) => {
 
 
     return(
-        <section>
-            <h1>{categoria} {equipo} [{tipo}] - {jugador}</h1>
-            <img src={img}/>
-            <p>{precio}</p>
-            <p>{descripcion}</p>
+        <section className="Detalle">
+            <div>
+                <h1 className="Detalle__titulo">{categoria} {equipo} [{tipo}] - {jugador}</h1>
+                <img src={img} className="Detalle__img"/>
+                <p className="Detalle__precio">{precio}</p>
+                <p className="Detalle__descripcion">{descripcion}</p>
+            </div>
 
+            <div>
+                <button type='button'>COMPRAR AHORA</button>
+            </div>
         </section>
     )
 }
